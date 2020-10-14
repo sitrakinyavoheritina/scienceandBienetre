8a46fa8ea3955e7a580e9ff195a9c50906429278
sitrakanyavo <sitrakinyavoheritina@gmail.com>
2020-10-14 09:58:51 +0300

    edit migration

---
+++ database/migrations/2020_10_12_065200_create_pack_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pack', function (Blueprint $table) {
+            // $table->increments('id_pack');
+            $table->id();
+            $table->string('desc_pack')->nullable();
+            $table->string('nom_pack')->nullable();
+            $table->string('video_pack')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pack');
+    }
+}
---
+++ database/migrations/2020_10_12_092321_create_conseil_pack_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConseilPackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conseil_pack', function (Blueprint $table) {
+            $table->id();
+            // $table->increments('id_conseil');
+            $table->string('conseil_photo');
+             $table->unsignedBigInteger('id_conseil_pack');
+            // $table->bigInteger('id_conseil_pack')->unsigned();
+            // $table-> unsignedBigInteger('id_conseil_pack')->onDelete('cascade');
+            $table->string('conseil_description');
+            // $table->foreign('id_conseil_pack')->references('id')->on('pack');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conseil_pack');
+    }
+}
