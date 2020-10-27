ae80bedb629eb4fcb1096aa16a8b6c7fa2c8e154
fredo <fredostanley@gmail.com>
2020-10-27 14:28:01 +0300

    header remove script

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -115,10 +115,10 @@
                                 <nav id="dropdown">
                                     <ul>
                         
-                                        <li><a href="contact.html">Accueil</a></li>
-                                        <li><a href="contact.html">Catalogue</a></li>
+                                        <li><a href="{{route('accueil')}}">Accueil</a></li>
+                                        <li><a href="{{url('listpack')}}">Catalogue</a></li>
                                     <!--     <li><a href="contact.html">contacts</a></li> -->
-                                        <li><a href="contact.html">A propos</a></li>
+                                        <li><a href="{{url('About')}}">A propos</a></li>
                                     </ul>
                                 </nav>
                             </div>                  
@@ -131,27 +131,7 @@
         </header>
  
         
-        <!-- all js here -->
-        <!-- jquery latest version -->
-        <script src="{{asset('public/assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
-        <!-- bootstrap js -->
-        <script src="{{asset('public/assets/js/bootstrap.min.js')}}"></script>
-        <!-- owl.carousel js -->
-        <!-- <script src="{{asset('public/assets/js/owl.carousel.min.js')}}"></script> -->
-          <!-- stellar js -->
-        <script src="{{asset('public/assets/js/jquery.stellar.min.js')}}"></script>
-        <!-- magnific js -->
-        <script src="{{asset('public/assets/js/magnific.min.js')}}"></script>
-        <!-- wow js -->
-        <script src="{{asset('public/assets/js/wow.min.js')}}"></script>
-        <!-- meanmenu js -->
-        <script src="{{asset('public/assets/js/jquery.meanmenu.js')}}"></script>
-        <!-- Form validator js -->
-        <!-- <script src="{{asset('public/assets/js/form-validator.min.js')}}"></script> -->
-        <!-- plugins js -->
-        <script src="{{asset('public/assets/js/plugins.js')}}"></script>
-        <!-- main js -->
-        <script src="{{asset('public/assets/js/main.js')}}"></script>
+     
     </body>
 
 <!-- Mirrored from rockstheme.com/rocks/aievari-live/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 03 Mar 2020 08:27:46 GMT -->
