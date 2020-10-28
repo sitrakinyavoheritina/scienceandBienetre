b979a0c32f7db759e71b498102bde53f101ddfd9
sitrakanyavo <sitrakinyavoheritina@gmail.com>
2020-10-28 15:18:34 +0300

    dfgdf

--- resources/views/payement.blade.php
+++ resources/views/payement.blade.php
@@ -7,82 +7,63 @@
             <div class="container">
                 <!-- Start counter Area -->
                 <div class="row ">
-                    <div class="fun-content">
-                        <div class="col-md-12 col-sm-12 col-xs-12">
-                            <!-- fun_text  -->
-                            <div class="fun_text backgr">
-                                 <h4 id="tilte_apropos">Information</h4>
-                            </div>
-                            <div class="ml-4 mt-4">
-                                <ul id="inline_list">
-                                    <li>
-                                        <a href="{{route('accueil')}}">Home
-                                        </a> 
-                                    </li>
-                                    <li>&nbsp; > &nbsp;Information</li>
-                                </ul>
-                                
-                            </div>
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="fun_text backgr">
+                             <h4 id="tilte_apropos">Information</h4>
+                        </div>
+                        <div class="ml-4 mt-4">
+                            <ul id="inline_list">
+                                <li>
+                                    <a href="{{route('accueil')}}">Home
+                                    </a> 
+                                </li>
+                                <li>&nbsp; > &nbsp;Information</li>
+                            </ul>
                             
-                        </div>              
-                    </div>
-
+                        </div>
+                    </div>              
                 </div>
             </br></br>
-            <div class="row">
-            	               <!-- Start Left contact -->
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                	<h4>etape 1</h4>
-                    <div class="contact-form">
-                        <div class="row">
-                            <form id="contactForm" method="POST" action="http://rockstheme.com/rocks/aievari-live/contact.php" class="contact-form">
-                                <div class="col-md-12 col-sm-12 col-xs-12">
-                                    <input type="text" id="name" class="form-control" placeholder="Nom" required data-error="Please enter your name">
-                                    <div class="help-block with-errors"></div>
-                                </div>
-                                <div class="col-md-12 col-sm-12 col-xs-12">
-                                    <input type="text" id="name" class="form-control" placeholder="Prenom" required data-error="Please enter your name">
-                                    <div class="help-block with-errors"></div>
-                                </div>
 
-                                <div class="col-md-12 col-sm-12 col-xs-12">
-                                    <input type="text" id="name" class="form-control" placeholder="Adresse E-mail" required data-error="Please enter your name">
-                                    <div class="help-block with-errors"></div>
-                                </div>
+<div class="row">
 
-                                <div class="col-md-12 col-sm-12 col-xs-12">
-                                    <input type="number" id="name" class="form-control" placeholder="Votre numero de téléphone" required data-error="Please enter your name">
-                                    <div class="help-block with-errors"></div>
-                                </div>
-                     
-                        
-                                <div class="col-md-12 col-sm-12 col-xs-12 text-center">
-                                    <button type="submit" id="submit" class="add-btn contact-btn">Send Message</button>
-                                    <div id="msgSubmit" class="h3 text-center hidden"></div> 
-                                    <div class="clearfix"></div>
-                                </div>   
-                            </form>  
-                        </div>
-                    </div>
+    <div class="col-md-6  col-sm-6 col-xs-12">    
+        <form>
+            <div class="form-group">
+                <label for="formGroupExampleInput">Nom</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
+              </div>
+              <div class="form-group">
+                <label for="formGroupExampleInput2">Prénoms</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="">
+              </div>
+              <div class="form-group">
+                <label for="formGroupExampleInput2">Adresse E-mail</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="">
 
-                </div>
-                <!-- End Left contact -->
-
-          	    <div class="col-md-6 col-sm-6 col-xs-12">
-          	    	<h4>liste pannier</h4>
-     			<hr>
-                    <div class="footer-content last-content">
-                        <div class="footer-head">
-                            <div class="footer-contacts">
-								<p><span>Pack geomincir :   </span>30€</p>
-								<p><span>Pack Sommeil : </span> 12€</p>
-								<p><span>Total   :</span> 42€</p>
-							</div> 
-                        </div>
+              </div>
+              <div class="form-group">
+                <label for="formGroupExampleInput2">Numéro téléphone</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="">
+              </div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                    <h4>liste pannier</h4>
+                <hr>
+                <div class="footer-content last-content">
+                    <div class="footer-head">
+                        <div class="footer-contacts">
+                            <p><span>Pack geomincir :   </span>30€</p>
+                            <p><span>Pack Sommeil : </span> 12€</p>
+                            <p><span>Total   :</span> 42€</p>
+                        </div> 
                     </div>
                 </div>
- 
             </div>
+</div>
+   
         </div>
     </div>
 @endsection
