b5b04e58aad0de6fecbde5da6e759304c877136c
fredo <fredostanley@gmail.com>
2020-10-28 10:11:50 +0300

    ajout tesxte a propos

--- resources/views/apropos.blade.php
+++ resources/views/apropos.blade.php
@@ -6,6 +6,8 @@
         position: initial ! important;
         padding: 56px 30px ! important;
     }
+   
+}
 
 </style>
 
@@ -37,16 +39,60 @@
                 </div>
             </br></br>
                 <p class="description_apropos">
-                    &nbsp;&nbsp;&nbsp;&nbsp;Science & Bien-Être est un laboratoire indépendant de recherche et développement dans le domaine du bien-être au naturel. Fort de 10 ans d’expérience, nos méthodes ont accompagné plus de 200 000 personnes dans plus de 80 pays.
+                    &emsp;Et si une bonne santé et un bien-être à toute épreuve étaient à portée de main sans gros effort et sans gros sacrifices ? Sur notre plateforme, nous vous les offrons facilement, pour ainsi dire, à prix bradé à travers des packs santé et bien-être très efficaces.  En effet, soucieux de vous satisfaire, le site a été mis en place afin de devenir votre partenaire en conseils pour votre quotidien. 
                 </p>
                 <p class="description_apropos">
-                    Que ce soit pour retrouver votre poids de forme, accéder à un meilleur sommeil, booster votre confiance en vous ou encore augmenter votre énergie au quotidien, nous proposons des solutions innovantes et radicales, en dehors des sentiers battus.
+                    &emsp;Le but étant de vous procurer du plaisir à vivre chaque instant de votre vie, nous mettons à votre disposition tout notre savoir, notre expertise et nos recherches afin de réaliser votre souhait d’une vie plaisante. En cela, plusieurs packs pratiques sont accessibles à partir du site, de quoi éveiller votre joie de vivre cachée.
+                </p>
+                <h4>
+                    <b>Santé et bien-être par son apparence
+                    </b>
+                </h4>
+                <p class="description_apropos">
+                    &emsp;Qui n’a pas déjà été victime d’une mauvaise critique quant à son physique au moins une fois dans sa vie ? Et quand cela s’est produit, quelle a été votre réaction première et votre ressenti ? Sans doute la peine, la déception, peut-être aussi la honte et la rage envers la personne qui a proféré ces bêtises, non ? Et c’est tout à fait normal puisque pour chacun d’entre-nous, le regard des autres est quand même assez important, que l’on s’en rende compte ou pas. 
                 </p>
                 <p class="description_apropos">
+                    &emsp;Mais si ces réactions sont faciles à adopter, elles ne sont pas moins dangereuses et néfastes pour notre santé. Mais pour les éviter, il y a deux solutions : d’une part, laisser passer et faire comme si de rien n’était en ayant un mental de fer et d’acier ; d’autre part, réagir en démontrant qu’on peut être tout le contraire de ce que les gens ont vu de nous jusqu’ici. Pour tous ceux qui choisissent ce second choix, notre site vous offre les outils pour y parvenir. 
+                </p>
+                <p class="description_apropos">
+                    &emsp;On parle, bien sûr d’apporter un changement notable à son physique. En d’autres termes, on adoptera une toute autre apparence, celle dont on a rêvé, celle que les autres ne pensaient pas qu’on puisse avoir un jour. Pour y parvenir, découvrez le panel de packs que propose notre site. Il y en a pour tous les goûts, et surtout pour tous les besoins : 
+                    <ul class="description_apropos"style="margin-left: 10rem">
+                        <li>-   le pack minceur pour ceux qui veulent affiner leur silhouette</li>
+                        <li>-   le pack prise de poids pour ceux qui, au contraire, veulent acquérir plus de volume </li>
+                        <li>-   le pack musculation pour ceux qui ont envie de gagner en musculature</li>
+                    </ul>
+                </p>
+                <p class="description_apropos">
+                   &emsp;Il est à noter que les résultats escomptés seront au rendez-vous et votre satisfaction par la même occasion. 
+                </p>
+                <h4>
+                    <b>Santé et bien-être par un cerveau bien entretenu
+                    </b>
+                </h4>
+                <p class="description_apropos">
+                    &emsp;Une bonne santé et un bien-être en profondeur sont deux notions qui s’acquièrent plus ou moins facilement selon les cas. Parfois, il suffira d’un petit travail sur soi ou d’une petite séance de yoga pour que les pensées négatives s’éloignent. Et d’autres fois, on peut avoir plus de difficultés à en bénéficier. Mais une fois acquis, il est aussi possible de les perdre avec beaucoup de facilité. Le plus difficile sera alors de les conserver. 
+                </p>
+                <p class="description_apropos">
+                    &emsp;Par une approche toute particulière, notre site vous permettra d’avoir toutes les cartes en mains pour pouvoir sauvegarder votre santé et votre bien-être sur le long terme. Le pack sommeil et entretien du cerveau a été élaboré juste à cet effet. Pourquoi cette approche plutôt spéciale ? Tout simplement parce qu’un cerveau en bonne santé aura pour reflet un bien-être psychologique et une bonne santé physique. Oui, mais pourquoi le sommeil ? Et bien, parce que la santé du cerveau vient aussi du sommeil tout comme celle de notre organisme. 
+                </p>
+                <p class="description_apropos">
+                    &emsp;A partir de ce pack, on agira en profondeur, à la base de ce qui pourra inspirer de la positivité dans notre quotidien : le cerveau. 
+                </p>
+                <h4>
+                    <b>Santé et bien-être par un organisme sain
+                    </b>
+                </h4>
+                <p class="description_apropos">
+                    &emsp;Après s’être occupé de son apparence, puis de son cerveau, qu’est-ce qui manque à un bien-être complet et une santé exceptionnelle ? La santé à proprement parler, bien sûr. D’ailleurs, ne dit-on pas que la santé est la plus grande richesse que l’on puisse avoir ? En ce sens, en prendre soin, s’en occuper et faire en sorte de la conserver est primordial pour acquérir un bien-être effectif. 
+                </p>
+                <p class="description_apropos">
+                    &emsp;Nous qui avons mis au point ce site l’avons bien compris et c’est pour vous faciliter la tâche dans cette démarche que le dernier pack a été réalisé : boost du système immunitaire. En effet, quel plaisir aurait-on de se retrouver alité à cause d’un gros rhume - sauf le fait de pouvoir manquer une journée de travail ou d’école, soit dit en passant ? Et que gagnerait-on à passer une journée sans travailler et qui pourrait ne pas être payée par notre employeur ? Des préoccupations supplémentaires, n’est-ce pas ? 
+                </p>
+                <p class="description_apropos">
+                    &emsp;En ce sens, le pack sur le système immunitaire sera sans doute le bienvenu dans votre petite collection d’exercices de bien-être et de santé, fournie par notre plateforme, évidemment. 
+                </p>
 
-                Notre unité de recherche est adossée à notre société mère : les éditions de la main bleue.
-                </p>
-                <p>&nbsp;</p>
+                <p>&emsp;</p>
                 <p>&nbsp;</p>
                 
             </div>
