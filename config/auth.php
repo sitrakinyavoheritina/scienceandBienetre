fb2926d2e6aa51b2f8de06821438d729691f93fd
sitrakanyavo <sitrakinyavoheritina@gmail.com>
2020-10-30 14:54:47 +0300

    api rest basic nety

--- config/auth.php
+++ config/auth.php
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'passeport',
             'provider' => 'users',
             'hash' => false,
         ],
