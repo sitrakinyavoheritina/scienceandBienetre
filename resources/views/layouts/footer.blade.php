a6bdb10ba8c87c02ca875491b3dc2bf1a2343539
sitrakanyavo <sitrakinyavoheritina@gmail.com>
2020-10-30 08:46:10 +0300

    ameliore performance

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,22 +4,12 @@
 ?>
 
         <!-- Start Footer Area -->
-        <script src="{{asset('public/assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
         <!-- bootstrap js -->
         <script src="{{asset('public/assets/js/bootstrap.min.js')}}"></script>
         <!-- owl.carousel js -->
-        <script src="{{asset('public/assets/js/owl.carousel.min.js')}}"></script>
-          <!-- stellar js -->
-        <script src="{{asset('public/assets/js/jquery.stellar.min.js')}}"></script>
-        <!-- magnific js -->
-        <script src="{{asset('public/assets/js/magnific.min.js')}}"></script>
-        <!-- wow js -->
-        <script src="{{asset('public/assets/js/wow.min.js')}}"></script>
-        <!-- meanmenu js -->
-        <script src="{{asset('public/assets/js/jquery.meanmenu.js')}}"></script>
-        <!-- Form validator js -->
-        <script src="{{asset('public/assets/js/form-validator.min.js')}}"></script>
-        <!-- plugins js -->
+
         <script src="{{asset('public/assets/js/plugins.js')}}"></script>
         <!-- main js -->
         <script src="{{asset('public/assets/js/main.js')}}"></script>
